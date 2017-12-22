e2ae3a0586774b803d175504a988c67e4a171ac5
Salvador Guzman <guzmansalv@gmail.com>
2017-12-22 10:18:42 -0800

    push data to newxt page

--- src/pages/uber-cookie/uber-cookie.ts
+++ src/pages/uber-cookie/uber-cookie.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Subject} from "rxjs/Subject";
 import "rxjs/add/operator/mergeMap";
 import {UberDataProvider} from "../../providers/uber-data/uber-data";
+import {UberMapPage} from "../uber-map/uber-map";
 
 @IonicPage()
 @Component({
@@ -12,15 +13,19 @@ import {UberDataProvider} from "../../providers/uber-data/uber-data";
 })
 export class UberCookiePage {
   cookie: string = '';
+  data: any;
   submitSubject = new Subject<string>();
   constructor(public navCtrl: NavController, public navParams: NavParams, private uberD: UberDataProvider) {
     this.submitSubject.flatMap(s => this.uberD.get(s)).subscribe(
-      s => console.log(s)
+      s => this.data = s
     )
   }
 
   public submit() {
     this.submitSubject.next(this.cookie);
+    this.navCtrl.push(UberMapPage, {
+      data: this.data
+    })
   }
 
   public static ionViewDidLoad() {
