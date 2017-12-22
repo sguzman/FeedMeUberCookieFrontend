34bcec270f186e972bf6fd1c1772a2edfd53cfad
Salvador Guzman <guzmansalv@gmail.com>
2017-12-22 09:41:43 -0800

    start with uber login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {UberCookiePage} from "../pages/uber-cookie/uber-cookie";
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
+export class MyApp  {
+  rootPage :any = UberCookiePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
